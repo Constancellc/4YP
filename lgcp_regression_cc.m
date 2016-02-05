b48b8084933b485511e7777bbea68b8851f6d4c1
Constancellc <constancellc@gmail.com>
2016-02-05 13:33:15 +0000

    Update lgcp_regression_cc.m
    changed plotting format and cleaned up bits i'm not using anymore

--- lgcp_regression_cc.m
+++ lgcp_regression_cc.m
@@ -1,4 +1,4 @@
-function [counts,mean_plus,mean_minus,t] = lgcp_regression_cc
+function [counts,variance,t] = lgcp_regression_cc
 
 % Importing the no. of deaths / day data
 direct_deaths = csvread('Data/Direct Frequencies.csv');
@@ -17,7 +17,6 @@ indirect_sampled = zeros(newlength,1);
 
 % Working out the necessary sampling frequency
 inte = floor(length(direct_deaths)/newlength);
-
 
 % Filling the sampled vector
 for i = 1:newlength
@@ -54,10 +53,11 @@ end
 % Find the log-average for each stream
 mean = [sum(logy(:,1)),sum(logy(:,2))]/newlength;
 
-% Turning on gradient search for the poisson rates, the tolerance is
-% reduced in order to speed up optimisation.
 %options = optimoptions('fminunc','Algorithm','trust-region','GradObj','on','DerivativeCheck','on');
-options = optimoptions('fminunc','Algorithm','trust-region','GradObj','on','TolX',1e-2);
+
+% Turning on gradient search and reducing the tolerance for the first round
+% of 'rough' optiisations
+options = optimoptions('fminunc','Algorithm','trust-region','GradObj','on','TolX',100);
 
 % This is to suppress the output of the optimizer. It's irritating.
 %options.Display = 'off';
@@ -65,84 +65,150 @@ options = optimoptions('fminunc','Algorithm','trust-region','GradObj','on','TolX
 % This is a function that limits its output between -1 and 1, it will be
 % used to constrain the correlation parameter.
 logit = @(x) (2./(1+exp(-x)))-1;
-ilogit = @(x) -log((2./(x+1))-1);            % Inverse of function
-dlogit = @(x) (1-x.^2)./(2);                 % Derivative of function
+ilogit = @(x) -log((2./(x+1))-1);          % Inverse of function
+dlogit = @(x) (1-x.^2)./(2);               % Derivative of function
 
-% This is the number of starting points we are going to try for our
+% This section defines the starting points for the first 'rough' round of
 % optimisation.
-num = 1;
 
-% Chose a precision for the trial start points of the optimisation
-precision = 10^-4;
+num = 30;                                   % Number of start points
+precision = 10^-4;                          % Precision of start points
 steps = 1/precision;
 
-% Define the possible start values for the output scales
-h_upper = 2;                     % Maximum value
-h = [h_upper/steps:h_upper/steps:h_upper];
+% Constraining the locations of the selected start points
+h_upper = 2;                                % Maximum height start point
+l_lower = 0;                               % Lowest length start point
+l_upper = 400;                              % Maximum lengthstart point
+n_upper = 0.01;                             % Maximum noise start point
 
-% Define the possible start values for the input scale
-l_lower = 20;                    % Lowest value
-l_upper = 220;                   % Maximum value
-l_step = (220-20)/steps;         
-l = [l_lower+l_step:l_step:l_upper];
-
-% Define the possible start values for the noise values
-noise_upper = 0.01;
-n = [noise_upper/steps:noise_upper/steps:noise_upper];
-
-% Define the possible start values for the correlation parameter
+% Compiling vectors of possible start points
+h = [h_upper/steps:h_upper/steps:h_upper];  % Height
+l_step = (l_upper-l_lower)/steps;         
+l = [l_lower+l_step:l_step:l_upper];        % Length
+n = [n_upper/steps:n_upper/steps:n_upper];  % Noise
 c = [-1:2/steps:1];
-c = c(2:end);
+c = c(2:end);                               % Correlation
 
 % Setting up the latin hyper-cube, containing numbers from 0 to the #steps
 X = steps*lhsdesign(num,6);
+
+best = 10000;
 
 % Rounding all entries to integer numbers
 for i = 1:num
     for j = 1:6
         X(i,j) = round(X(i,j));
+        if X(i,j) == 0
+            X(i,j) = 1;
+        end
     end
     
     % Fill array 'samples' with values from the predefined vectors for each
     % parameter
-    samples(i,1) = h(X(i,1));
-    samples(i,2) = h(X(i,2));
-    samples(i,3) = l(X(i,3));
-    samples(i,4) = n(X(i,4));
-    samples(i,5) = n(X(i,5));
-    samples(i,6) = ilogit(c(X(i,6)));
-end
+    samples(1) = log(h(X(i,1)));
+    samples(2) = log(h(X(i,2)));
+    samples(3) = l(X(i,3));
+    samples(4) = log(n(X(i,4)));
+    samples(5) = log(n(X(i,5)));
+    samples(6) = ilogit(c(X(i,6)));
 
-best = 0;
-
-for i = 1:num
+   
+    try [x,fval] = fminunc(@product,[transpose(samples);logy(:,1);...
+            logy(:,2)],options);
+    catch 
+        n1 = exp(samples(4));
+        n2 = exp(samples(5));
+        
+        counter = 1;
+        epsilon = 1e-6;
+        
+        while counter <=1
+            samples(4) = log(n1 + epsilon);
+            samples(5) = log(n2 + epsilon);
+            
+            try [x,fval] = fminunc(@product,[transpose(samples);logy(:,1);...
+                    logy(:,2)],options);
+            catch
+                epsilon = epsilon*1.1
+                counter = counter - 1;
+            end
+            
+            counter = counter + 1;
+        end
+    end
     
-    [x,fval] = fminunc(@product,[transpose(samples(num,:));logy(:,1);...
-        logy(:,2)],options);
+    fval
     
     % If the function minimum is smaller than the previous best recorded
     if fval < best
-        best = fval;                      % Replace best
-        
-        rho1_final = (x(1));              % Replace hyper-parameters
-        rho2_final = (x(2));
-        l_final = (x(3));
-        s_final1 = exp(x(4));
-        s_final2 = exp(x(5));
-        a_final = logit(x(6));
-
-        v_ = x(7:end);                    % Replace poisson rates
-        % Duplicated for both data streams
-        v_final = [v_(1:newlength),v_(newlength+1:end)];
+        best = fval;                      % Replace best        
+        x_ = x;
     end
 
-end       
+end
 
+%%%%%%%
 
-%[x,fval] = fminunc(@product,[0.5;0.6;70;log(0.0001);log(0.0001);0.8;logy(:,1);logy(:,2)],options);
+x=x_;
 
-%rho1_final = exp(x(1));
-%rho2_final = exp(x(2));
+rho1_final = exp(x(1));              % Replace hyper-parameters
+rho2_final = exp(x(2));
+l_final = (x(3));
+s_final1 = exp(x(4));
+s_final2 = exp(x(5));
+a_final = logit(x(6));
+
+% The following section is needed when we want to plot the output of the
+% initial round of optimisation
+%{
+vi = x(7:end);
+vi_final = [vi(1:newlength),vi(newlength+1:end)];
+
+vari = hessian_diag(vi);
+
+% Calculating the variance terms
+variancei = [vari(1:newlength),vari(newlength+1:end)];
+
+% Working out vectors of the mean +/- 2 std 
+mean_plusi = zeros(2,newlength);
+mean_minusi = zeros(2,newlength);
+
+for k = 1:newlength
+    for h = 1:2
+        mean_plusi(h,k) = exp(vi_final(k,h) + 2*sqrt(variancei(k,h)));
+        mean_minusi(h,k) = exp(vi_final(k,h) - 2*sqrt(variancei(k,h)));
+    end
+end
+
+Y1i = [mean_minusi(1,:),fliplr(mean_plusi(1,:))];
+Y2i = [mean_minusi(2,:),fliplr(mean_plusi(2,:))];
+
+%}
+
+%%%%%%%
+
+best
+
+% {
+optionsnew = optimoptions('fminunc','Algorithm','trust-region','GradObj','on','TolFun',1e-6);
+%optionsnew = optimoptions('fminunc','Algorithm','trust-region','GradObj','on');
+[x,fval] = fminunc(@product,x_,optionsnew);
+%}
+
+%x=x_;
+rho1_final = exp(x(1));              % Replace hyper-parameters
+rho2_final = exp(x(2));
+l_final = (x(3));
+s_final1 = exp(x(4));
+s_final2 = exp(x(5));
+a_final = logit(x(6));
+
+v_ = x(7:end);                    % Replace poisson rates
+
+% Split into seperate data streams
+v_final = [v_(1:newlength),v_(newlength+1:end)];
+
+fval
 
 var = hessian_diag(v_);
 
@@ -180,8 +246,32 @@ X = [t,fliplr(t)];
 Y1 = [mean_minus(1,:),fliplr(mean_plus(1,:))];
 Y2 = [mean_minus(2,:),fliplr(mean_plus(2,:))];
 
+
+% Remove Anomili
+indirect_deaths(1328) = 0;
+direct_deaths(1328) = 0;
+
 figure                                % Create a new figure
 
+% This is the first subplot if we want the output of the initial round
+%{
+subplot(2,1,1)
+shade = fill(X,Y1i,'b');               % Fills the confidence region   
+set(shade,'facealpha',.2)             % Sets the region transparent
+set(shade,'EdgeColor','None')
+hold on
+shade2 = fill(X,Y2i,'g');              
+set(shade2,'facealpha',.2)           
+set(shade2,'EdgeColor','None')
+plot(t,exp(vi_final))             % Plots the predicted mean values
+hold on
+plot(t,y,'x');                   % Plots the training data
+
+subplot(2,1,2)
+%}
+
+% This is if we want to plot the test and training data
+subplot(2,1,2)
 shade = fill(X,Y1,'b');               % Fills the confidence region   
 set(shade,'facealpha',.2)             % Sets the region transparent
 set(shade,'EdgeColor','None')
@@ -194,8 +284,19 @@ hold on
 plot(t,y,'x');                   % Plots the training data
 
 
-
-%p = posterior(v_final(:,1))
+subplot(2,1,2)
+shade = fill(X,Y1i,'b');               % Fills the confidence region   
+set(shade,'facealpha',.2)             % Sets the region transparent
+set(shade,'EdgeColor','None')
+hold on
+shade2 = fill(X,Y2i,'g');              
+set(shade2,'facealpha',.2)           
+set(shade2,'EdgeColor','None')
+plot(t,exp(vi_final))             % Plots the predicted mean values
+hold on
+plot(days,direct_deaths,'b');
+hold on
+plot(days,indirect_deaths,'g');
 
 %}
 %-------------------------------
@@ -205,58 +306,14 @@ plot(t,y,'x');                   % Plots the training data
 
 
 %-------------------------------------------------------
-% START OF HYPER-PARAMETERS LIKELIHOOD PLOTTING SECTION
+% START OF LOCAL FUNCTION SECTION
 %-------------------------------------------------------
-%{
 
-% Setting up the vectors of hyperparameters to be plotted
-h = [0.01:0.01:20];
-h_test = zeros(length(h),2);
-
-l = [0.1:0.1:500];
-l_test = zeros(length(l),2);
-
-n = [0.000001:0.000001:0.0001];
-n_test = zeros(length(n),2);
-
-% Working out the likelihood for each test parameter
-for q = 1:2;
-    v_test = v_final(:,q);
-
-    for i = 1:length(l)
-        l_test(i,q) = exp(-hyper([h_final(q);l(i);s_final(q)]));
-    end
-
-    for i = 1:length(h)
-        h_test(i,q) = exp(-hyper([h(i);l_final(q);s_final(q)]));
-    end
-
-    for i = 1:length(n)
-        n_test(i,q) = exp(-hyper([h_final(q);l_final(q);n(i)]));
-    end
-end
-
-figure                             % Plotting the variation for all three
-subplot(2,2,1)
-semilogy(h,h_test)
-
-subplot(2,2,2)
-semilogy(l,l_test)
-
-subplot(2,2,3)
-semilogy(n,n_test)
-
-%}
-%-----------------------------------------------------
-% END OF HYPER-PARAMETERS LIKELIHOOD PLOTTING SECTION
-%-----------------------------------------------------
 
 % Function which calculates selected terms of the posterior numerator.
     function [f,g] = product(x)
-        rho_1 = (x(1));                % Direct output scale
-        rho_2 = (x(2));                % Indirect output scale
-        %rho_1 = exp(x(1));
-        %rho_2 = exp(x(2));
+        rho_1 = exp(x(1));
+        rho_2 = exp(x(2));
         x_l = (x(3));                  % Length scale
         x_s1 = exp(x(4));              % Direct noise variance
         x_s2 = exp(x(5));              % Indirect noise variance
@@ -291,8 +348,8 @@ semilogy(n,n_test)
         covdr1 = [2*rho_1*covk,a*rho_2*covk;a*rho_2*covk,zeros(newlength,newlength)];
         covdr2 = [zeros(newlength,newlength),a*rho_1*covk;a*rho_1*covk,2*rho_2*covk];
         
-        gr1 = 0.5*(-transpose(d)*(R\(L\covdr1))*(R\(L\d))+trace(R\(L\covdr1)));
-        gr2 = 0.5*(-transpose(d)*(R\(L\covdr2))*(R\(L\d))+trace(R\(L\covdr2)));
+        gr1 = 0.5*rho_1*(-transpose(d)*(R\(L\covdr1))*(R\(L\d))+trace(R\(L\covdr1)));
+        gr2 = 0.5*rho_2*(-transpose(d)*(R\(L\covdr2))*(R\(L\d))+trace(R\(L\covdr2)));
                 
         % Next the gradient for the length scale
         for ci = 1:newlength
@@ -331,19 +388,14 @@ semilogy(n,n_test)
         noise1 = s_final1*eye(newlength,newlength);
         noise2 = s_final2*eye(newlength,newlength);
         
-        %covk = covk+noise;
-        
-        %cov_ = [(rho1_final^2)*covk+noise,rho1_final*rho2_final*covk+noise;...
-        %    rho1_final*rho2_final*covk+noise,(rho2_final^2)*covk+noise];
-        
-        %cov_ = [(rho1_final^2)*covk,rho1_final*rho2_final*covk;...
-        %    rho1_final*rho2_final*covk,(rho2_final^2)*covk];
-        
         cov_ = [(rho1_final^2)*covk+noise1,a_final*rho1_final*rho2_final*covk;...
             a_final*rho1_final*rho2_final*covk,(rho2_final^2)*covk+noise2];
         
+        % Cholesky decomposition to avoid inverting covariance
+        L = chol(cov_,'lower');
+        R = transpose(L);
         
-        icov = cov_\eye(size(cov_));
+        icov = R\(L\eye(2*newlength));
         
         A = eye(2*newlength);
         
@@ -362,52 +414,6 @@ semilogy(n,n_test)
         va = diag(covariance);
     end
 
-%{
-    function p = posterior(v)
-        
-        cov = cov_matrix(x2,x2,h_final(q),l_final(q)) +... 
-            (s_final(q)+10^-9)*eye(newlength);
-        
-        icov = cov\eye(size(cov));
-        
-        d = v-mean(q)*ones(newlength,1);
-        
-        y_fact = 1;
-        
-        for yi = 1:newlength
-            y_fact = y_fact/factorial(y(yi,q));
-        end
-        
-        log_product = -sum(exp(v))+transpose(v)*y(:,q)+log(y_fact)-...
-            (newlength/2)*log(2*pi)-0.5*log(det(cov))-0.5*transpose(d)*(cov\d);
-        
-        product = exp(log_product);
-        
-        v0 = v_final(:,q);
-        d0 = v0-mean(q)*ones(newlength,1);
-        
-        log_p0 = -sum(exp(v0))+transpose(v0)*y(:,q)+log(y_fact)-...
-            (newlength/2)*log(2*pi)-0.5*log(det(cov))-0.5*transpose(d0)*(cov\d0);
-        p0 = exp(log_p0);
-        
-        A = eye(newlength);
-        
-        for ai = 1:newlength
-            for aj = 1:newlength
-                if ai == aj
-                    A(ai,aj) = exp(v(ai)) + icov(ai,aj);
-                else
-                    A(ai,aj) = 0.5*(icov(ai,aj) + icov(aj,ai));
-                end
-            end
-        end
-        
-        factor = p0*sqrt(((2*pi)^newlength)/det(A));
-        
-        p = product / factor;
-    end
-
-%}
 end
 
 
